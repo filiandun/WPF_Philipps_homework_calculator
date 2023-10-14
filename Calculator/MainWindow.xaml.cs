d080488d98aab2b1ccb6f417b2479160fc3f9f8c
filian <filiandun@gmail.com>
2023-10-14 20:28:48 +0300

    project 100% ready

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,141 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool clear;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.clear = false;
+        }
+
+
+        // КНОПКИ УПРАВЛЕНИЯ
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.numTextBox.Text != "0")
+            {
+                this.numTextBox.Text = this.numTextBox.Text.Remove(this.numTextBox.Text.Length - 1, 1);
+                if (this.numTextBox.Text == "")
+                {
+                    this.numTextBox.Text = "0";
+                }
+            }
+        }
+
+        private void CleanEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.numTextBox.Text = "0";
+        }
+
+        private void CleanButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.expTextBox.Text = string.Empty;
+            this.numTextBox.Text = "0";
+        }
+
+
+        // КНОПКИ С ЧИСЛАМИ
+        private void NumButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Clear();
+
+            if (sender is Button button)
+            {
+                if (this.numTextBox.Text == "0" && button.Content.ToString() != ",")
+                {
+                    this.numTextBox.Text = button.Content.ToString();
+                }
+                else
+                {
+                    this.numTextBox.Text += button.Content.ToString();
+                }
+            }
+        }
+
+
+        // КНОПКИ С МАТЕМАТИЧЕСКИМИ ОПЕРАЦИЯМИ
+        private void SymButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Clear();
+
+            if (sender is Button button)
+            {   
+                if (string.IsNullOrEmpty(this.expTextBox.Text))
+                {
+                    this.expTextBox.Text += this.numTextBox.Text + " "; // добавление введённого пользователем числа
+                    this.expTextBox.Text += button.Content.ToString() + " "; // добавление математического знака
+
+                    this.numTextBox.Text = "0"; // обнуление введённого пользователям числа
+                }
+                else
+                {
+                    this.ResultButton_Click(sender, e);
+                    this.SymButton_Click(sender, e);
+                }
+            }
+        }
+
+
+        // КНОПКА РЕЗУЛЬТАТ
+        private void ResultButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(this.expTextBox.Text))
+            {
+                string[] expTextBoxStr = this.expTextBox.Text.Split(' ');
+
+                decimal firstNum = Convert.ToDecimal(expTextBoxStr[0]); // получение первого числа
+                decimal secondNum = Convert.ToDecimal(this.numTextBox.Text); // получение второго числа
+                string mathSymbol = expTextBoxStr[1]; // получение математического знака
+
+                try
+                {
+                    decimal result;
+                    switch (mathSymbol)
+                    {
+                        case "/": result = firstNum / secondNum; break;
+                        case "*": result = firstNum * secondNum; break;
+                        case "-": result = firstNum - secondNum; break;
+                        case "+": result = firstNum + secondNum; break;
+
+                        default: result = 0; break;
+                    }
+
+                    this.expTextBox.Text += secondNum + " =";
+                    this.numTextBox.Text = this.Round(result).ToString();
+                }
+                catch (DivideByZeroException)
+                {
+                    System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=Dd2QvzpM8ss");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                this.clear = true;
+            }
+        }
+
+
+        // ВСОМОГАТЕЛЬНЫЕ МЕТОДЫ
+        private void Clear()
+        {
+            if (this.clear)
+            {
+                this.expTextBox.Text = string.Empty;
+                this.clear = false;
+            }
+        }
+
+        private decimal Round(decimal number) // нужно, чтобы в случае, например, 20,1 - 0,1 = 20,0 - не таскать этот ноль бесячий
+        {
+            if (number == Math.Truncate(number))
+            {
+                return Math.Truncate(number);
+            }
+            return number;
         }
     }
 }
